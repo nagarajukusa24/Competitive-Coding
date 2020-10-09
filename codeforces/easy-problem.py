3a055cc37eb64f164961e1b46b96924d69f957eb
nagarajukusa24 <nagarajukusa24@gmail.com>
2020-10-09 22:27:00 +0530

    New solutions added

---
+++ codeforces/easy-problem.py
@@ -0,0 +1,7 @@
+n = int(input())
+li = list(map(int, input().split(" ")))
+k = 1
+if k in li:
+	print("HARD")
+else:
+	print("EASY")
---
+++ codeforces/hulk.py
@@ -0,0 +1,7 @@
+n = int(input())
+li = ["I hate","I love"]
+str = ""
+for i in range(n):
+	if(i%2==0):
+		str = str + li[0]
+	
